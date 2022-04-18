ec46578d37a23ed8fef635ec34e9c74d17eda430
Yousef Samara <yousef.aa.samara@gmail.com>
2022-04-18 15:16:12 +0300

    allsolved

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -4,20 +4,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Text.Json;
+using Microsoft.Azure.Amqp.Framing;
+using Newtonsoft.Json;
 
 namespace LINQ
 {
     class Program
     {
-        static void Main(string[] args)
+        //public static List<Person> Source { get; private set; }
+
+        public static void Main(string[] args)
         {
             string path = @"../../../data.json";
             string jsonData = File.ReadAllText(path);
-            Console.WriteLine(jsonData);
+                //Console.WriteLine(jsonData);
+            dataRoot data = JsonConvert.DeserializeObject<dataRoot>(jsonData);
+
+            Console.WriteLine("All neighborhoods");
+            Console.WriteLine("-----------------------------------------");
+            allNeighborhood(data);
+            Console.WriteLine("-----------------------------------------");
+
+
+            Console.WriteLine("All neighborhoods with nams");
+            Console.WriteLine("-----------------------------------------");
+            allNeighborhoodWithNames(data);
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("All neighborhoods with nams with no Duplicates ");
+            Console.WriteLine("-----------------------------------------");
+            allNeighborhoodNoDuplicates(data);
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("single query.");
+            Console.WriteLine("-----------------------------------------");
+            singleQuery(data);
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("Rewrite using Use LINQ Query.");
+            Console.WriteLine("-----------------------------------------");
+            rewriteSingleQuery(data);
+            Console.WriteLine("-----------------------------------------");
+
+
+
         }
 
-        public class Root
+        public static void allNeighborhood(dataRoot data)
+        {
+            var all = from Feature in data.features
+                     select Feature.properties.neighborhood;
+            int countar = 1;
+            foreach (var x in all)
+            {
+                Console.WriteLine($"{countar}. {x}"); 
+                countar++;
+            }
+        }
+
+        public static void allNeighborhoodWithNames(dataRoot data)
+        {
+            var all = from Feature in data.features
+                      where Feature.properties.neighborhood != ""
+                      select Feature.properties.neighborhood;
+            int countar = 1;
+            foreach (var x in all)
+            {
+                Console.WriteLine($"{countar}. {x}");
+                countar++;
+            }
+        }
+
+        public static void allNeighborhoodNoDuplicates(dataRoot data)
+        {
+            var all = from Feature in data.features
+                      where Feature.properties.neighborhood != ""
+                      select Feature.properties.neighborhood;
+            var y =all.Distinct();
+            int countar = 1;
+            foreach (var x in y)
+            {
+                Console.WriteLine($"{countar}. {x}");
+                countar++;
+            }
+        }
+
+        public static void singleQuery(dataRoot data)
+        {
+            var all = from Feature in data.features where Feature.properties.neighborhood != "" select Feature.properties.neighborhood;
+            var y = all.Distinct();
+            int countar = 1;
+            foreach (var x in y)
+            {
+                Console.WriteLine($"{countar}. {x}");
+                countar++;
+            }
+        }
+
+        public static void rewriteSingleQuery(dataRoot data)
+        {
+            //var all = from Feature in data.features where Feature.properties.neighborhood != "" select Feature.properties.neighborhood;
+            //var y = all.Distinct();
+            var allN = data.features.Select(x => x.properties.neighborhood).Where(y => y != "").Distinct();
+            int countar = 1;
+            foreach (var x in allN)
+            {
+                Console.WriteLine($"{countar}. {x}");
+                countar++;
+            }
+        }
+
+
+
+
+
+        public class dataRoot
         {
             public string type { get; set; }
             public List<Feature> features { get; set; }
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Lab09-LINQ
+# Summary :
+
+this program give you the ability to read data from a json file and filter the neighborhoods whith several ways.
+
+this Program have some useful queries that used for several filters :
+
+1. print all of the neighborhoods in data.
+2.a Filter that will filter the neighborhoods that have no names.
+3.a Filter that will filter the neighborhoods that have no names and remove all duplicates.
+4.consolidate all into one single query.
+5.Rewrite the query with LINQ method.
+
+## RUN :
+
+![image](run.png)
+
+
+
+
+
+
